1d51aa9c3246d326e86ac61131ae960e4928ef0a
Jeremy McRae <jmcrae@illumina.com>
2018-03-16 17:07:45 -0700

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup (name="peds",
     description='Package for parsing pedigree files',
     long_description=io.open('README.rst', encoding='utf-8').read(),
-    version="1.3.1",
+    version="1.3.2",
     author="Jeremy McRae",
     author_email="jmcrae@illumina.com",
     license="MIT",
